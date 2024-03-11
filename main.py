f5592fb5602b593bb66967c387bf67fc58ffce66
donngai <donovanngai237@gmail.com>
2024-03-11 15:06:22 -0400

    Moved database for task1 into seperate file

--- main.py
+++ main.py
@@ -1,15 +1,2 @@
-# Create the basics of the database
+#Main file for all the task
 import pandas as pd
-
-#Task 1
-debtDB = pd.DataFrame()
-debtDB['Due Date'] = []
-debtDB['Expense'] = []
-debtDB['Remaining Balance'] = []
-debtDB['Month Overdue'] = []
-
-def main():
-    print(debtDB)
-
-if __name__ == "__main__":
-    main()
